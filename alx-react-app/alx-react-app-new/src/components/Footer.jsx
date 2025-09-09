fb0a136656aed9c93f7ebed013131926b2310e31
MustBeBang <808Junkiez.kyriebeatz@gmail.com>
2025-09-09 15:34:52 +0200

    Added new files

---
+++ alx-react-app/alx-react-app-new/src/components/Footer.jsx
@@ -0,0 +1,19 @@
+function Footer() {
+  return (
+    <footer style={{
+      backgroundColor: '#333',
+      color: '#fff',
+      textAlign: 'center',
+      padding: '15px 0',
+      position: 'relative',
+      bottom: 0,
+      width: '100%',
+      marginTop: '20px'
+    }}>
+      <p style={{ margin: 0 }}>© 2025 My Company. All rights reserved.</p>
+      <p style={{ fontSize: '12px', margin: '5px 0 0 0' }}>Follow us on social media for updates!</p>
+    </footer>
+  );
+}
+
+export default Footer;
